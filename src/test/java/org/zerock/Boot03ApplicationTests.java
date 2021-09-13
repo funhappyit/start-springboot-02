78a7964307ede88653e90f753120a065eaa2a7f3
funhappyit <fun.happyit@gmail.com>
2021-09-13 23:31:37 +0900

    no message

--- src/test/java/org/zerock/Boot03ApplicationTests.java
+++ src/test/java/org/zerock/Boot03ApplicationTests.java
@@ -1,11 +1,29 @@
 package org.zerock;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.domain.Board;
+import org.zerock.persistence.BoardRepository;
 
 @SpringBootTest
 class Boot03ApplicationTests {
 
+	@Autowired
+	private BoardRepository repo;
+	
+	@Test
+	public void testInsert200() {
+		for(int i=1;i<=200;i++) {
+			Board board = new Board();
+			board.setTitle("제목.."+i);
+			board.setContent("내용..."+i+"채우기");
+			board.setWriter("user0"+(i%10));
+			repo.save(board);
+		}
+	}
+	
+	
 	@Test
 	void contextLoads() {
 	}

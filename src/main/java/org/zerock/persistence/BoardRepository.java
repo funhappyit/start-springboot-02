eb3782d5b4763427bc68ef4e3767d375e7aae26b
funhappyit <fun.happyit@gmail.com>
2021-09-14 18:13:51 +0900

    2021-09-14 1)페이지 네이션

--- src/main/java/org/zerock/persistence/BoardRepository.java
+++ src/main/java/org/zerock/persistence/BoardRepository.java
@@ -1,8 +1,33 @@
 package org.zerock.persistence;
 
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.zerock.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long>{
-
+	public List<Board> findBoardByTitle(String title);
+	
+	public Collection<Board> findByWriter(String writer);
+	
+	//작성자에 대한 like %키워드%
+	public Collection<Board> findByWriterContaining(String writer);
+	
+	//OR 조건의 처리 
+	public Collection<Board> findByTitleContainingOrContentContaining(String title, String content);
+	
+	//title LIKE % ? % AND BNO > ?
+	public Collection<Board> findByTitleContainingAndBnoGreaterThan(String keyword, Long num);
+	
+	//bno > ? ORDER BY bno DESC
+	public List<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno, Pageable paging);
+	
+//	public List<Board> findByBnoGreaterThan(Long bno,Pageable paging);
+	
+	public Page<Board> findByBnoGreaterThan(Long bno,Pageable paging);
+	
 }
--- src/test/java/org/zerock/Boot03ApplicationTests.java
+++ src/test/java/org/zerock/Boot03ApplicationTests.java
@@ -1,8 +1,16 @@
 package org.zerock;
 
+
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.domain.Board;
 import org.zerock.persistence.BoardRepository;
 
@@ -11,7 +19,7 @@ class Boot03ApplicationTests {
 
 	@Autowired
 	private BoardRepository repo;
-	
+	/*
 	@Test
 	public void testInsert200() {
 		for(int i=1;i<=200;i++) {
@@ -22,6 +30,55 @@ class Boot03ApplicationTests {
 			repo.save(board);
 		}
 	}
+
+	@Test
+	public void testByTitle() {
+		repo.findBoardByTitle("제목..177")
+		.forEach(board->System.out.println(board));
+	}
+	*/
+	/*
+	@Test
+	public void testByWriter() {
+		Collection<Board> results = repo.findByWriter("user00");
+		
+		results.forEach(
+				board -> System.out.println(board)
+		);
+	}
+	
+	@Test
+	public void testByWriterContaining() {
+		Collection<Board> results = repo.findByWriterContaining("05");
+		//for 루프 대신 forEach
+		results.forEach(board->System.out.println(board));
+		
+	}
+	
+	@Test
+	public void testBnoOrderBy() {
+		Pageable paging = PageRequest.of(0, 10);
+		Collection<Board> results = repo.findByBnoGreaterThanOrderByBnoDesc(0L,paging);
+		results.forEach(board->System.err.println(board));
+	}
+	*/
+	@Test
+	public void testBnoPagingSort() {
+		Pageable paging = PageRequest.of(0, 10, Sort.Direction.ASC, "bno");
+		
+		Page<Board> results = repo.findByBnoGreaterThan(0L, paging);
+		System.out.println("PAGE SIZE: "+results.getSize());
+		System.out.println("TOTAL PAGES: "+results.getTotalPages());
+		System.out.println("TOTAL COUNT: "+results.getTotalElements());
+		System.out.println("NEXT: "+results.getPageable());
+		
+		List<Board> list = results.getContent();
+		list.forEach(board->System.out.println(board));
+	}
+
+	
+	
+	
 	
 	
 	@Test
